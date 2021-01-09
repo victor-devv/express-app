3667fa3e89ab78578ded20dd2000aea0f557f901
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-01-09 13:30:41 +0100

    Filtering Routes

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Handling Routes in Express
 //using the get/post/delete etc do exact match as opposed to using use(), so arranging your routes doesn't matter when you use any of the protocol functions in your routes and not use()
+
 //Import routes
 
-app.use(adminRoutes);
+//You can filter routes by adding a prefix as shown below. Express would add the prefix before all paths in the route files
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
