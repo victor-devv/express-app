1354b4359ef3ab210b0ee1d92d09f648e4001140
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-01-09 12:57:40 +0100

    Handling Routes in Express

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// Add middleware used to parse body request
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Handling Routes in Express
 
 //If you wanna add a middleware that applies to all routes, it should be on top, with the next()
-app.use('/',(req, res, next) => {
-  console.log("Welcome to My Express App!");
-  next();
-});
+// app.use('/',(req, res, next) => {
+//   console.log("Welcome to My Express App!");
+//   next();
+// });
 
 //Note that if you're sending a response in a middleware, make sure not to add the next() function
-app.use('/add-product',(req, res, next) => {
-  res.send("<h2>Add Product Page</h2>");
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    "<h2>Add Product Page</h2></hr></br></br><form action='/add-product' method='POST'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
+  );
 }); //this route has to be placed before the / route cos routes are read from top to bottom and since there'll be a '/' before add-product, it'll read the / route instead.
 
+app.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h2>Hello From Express!</h2>");
 });
 
