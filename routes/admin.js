238ecd6a583c618d7c4db385f94457b0acdf734c
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-01-09 13:50:23 +0100

    Serving HTML Files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 //Note that if you're sending a response in a middleware, make sure not to add the next() function
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<h2>Add Product Page</h2></hr></br></br><form action='/admin/add-product' method='POST'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
-  );
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h2>Hello From Express!</h2>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html")); //same as '/views/shop.html' but with functionality to work on all systems, and dirname to path of the projects
 });
 
 module.exports = router;
