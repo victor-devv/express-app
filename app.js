f85881c577c28161c96359950191b9b9e5b7a3ca
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-01-09 12:26:35 +0100

    Middlewares with Express 2

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const http = require('http');
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //This allows the request to continue to the next middleware in line
+// Handling Routes in Express
+
+//If you wanna add a middleware that applies to all routes, it should be on top, with the next()
+app.use('/',(req, res, next) => {
+  console.log("Welcome to My Express App!");
+  next();
 });
 
-app.use((req, res, next) => {
+//Note that if you're sending a response in a middleware, make sure not to add the next() function
+app.use('/add-product',(req, res, next) => {
+  res.send("<h2>Add Product Page</h2>");
+}); //this route has to be placed before the / route cos routes are read from top to bottom and since there'll be a '/' before add-product, it'll read the / route instead.
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware!");
-  res.send("<h1>Hello From Express!</h1>");
+  res.send("<h2>Hello From Express!</h2>");
 });
 
-// const server = http.createServer(app);
 
-// server.listen(3000);
 app.listen(3000);
