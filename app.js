fe01e46d0a57bb728fbd0f84b0f9f444ef090472
VICTOR-TVL <vheecthurikuomola@gmail.com>
2021-01-09 14:07:24 +0100

    Helper function for root directory

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+
+const rootDir = require("./helpers/path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,7 +21,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found!</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "errors", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../helpers/path");
+
 const router = express.Router();
 
 //Note that if you're sending a response in a middleware, make sure not to add the next() function
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
